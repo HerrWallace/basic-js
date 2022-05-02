b6416e2fe8cbac99ae3bf06e4ad87888f339f023
Urban Vadzim <Herr.Wallace@protonmail.com>
2022-05-02 21:32:21 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(array) {
+  if (!Array.isArray(array)) {
+    return false;
+  }
+  let result = '';
+
+  let newArr = array
+  .filter((item) => typeof item === 'string')
+  .map((item) => {
+    for (let i = 0; i < item.length; i++) {
+      if (item[i] !== ' ') {
+        return item[i].toUpperCase();
+      }
+      
+    }
+  })
+  .sort();
+
+  for (item of newArr) {
+    result += item;
+  }
+  return result.toUpperCase();
 }
 
 module.exports = {
